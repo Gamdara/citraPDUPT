51568fabbb23ac53e03d1affc881a10c7fbe8e80
Gamdara <poiu12345654321@gmail.com>
2022-09-19 11:01:31 +0800

    auto map

--- generate point.py
+++ generate point.py
@@ -5,10 +5,10 @@ from qgis.core import (
     QgsVectorLayer
 )
 
-files = [f for f in os.listdir('.') ]
-for f in files:
-    print(f)
-
+#files = [f for f in os.listdir('.') ]
+#for f in files:
+#    print(f)
+#
 rumusGDAL = {
     "ndvi" : ' ( C - B )  /  ( C  + B) ',
     "ndbi" : ' ( D - C )  /  ( D  + C) ',
@@ -17,10 +17,10 @@ rumusGDAL = {
 }
 
 rumus = {
-    "ndvi" : ' ( B5 - B4)  /  ( B5  + B4) ',
-    "ndbi" : ' ( B6- B5 )  /  ( B6 + B5) ',
-    "ndisi" : '(B10 - 1/3(B2+B5+ B7 )) / (B10 + 1/3(B2+B5+ B7 ))',
-    "albedo" : '(0.356  *  B2+ 0.13 * B4+ 0.373 * B5 + 0.085 * B6+ 0.072 * B7 - 0.0018)'
+    "ndvi" : ' ( B5@1 - B4@1)  /  ( B5@1  + B4@1) ',
+    "ndbi" : ' ( B6@1- B5@1 )  /  ( B6@1 + B5@1) ',
+    "ndisi" : '(B10@1 - 1/3*(B2@1+B5@1+ B7@1 )) / (B10@1 + 1/3*(B2@1+B5@1+ B7@1 ))',
+    "albedo" : '(0.356  *  B2@1+ 0.13 * B4@1+ 0.373 * B5@1 + 0.085 * B6@1+ 0.072 * B7@1 - 0.0018)'
 }
 
 types = ['ndvi','ndbi','ndisi','albedo','lst']
@@ -37,10 +37,11 @@ dates = [
     '5/15/2021'
 ]
 
+QgsProject.instance().clear()
 entries = []
 folder = os.listdir("citra")[0]
 for file in os.listdir("citra"+"\\"+folder):
-    if file.startswith(folder+"_SR_B") or file.startswith(folder+"_ST_B"):
+    if file.startswith(folder+"_SR_B") or (file.startswith(folder+"_ST_B") and file.endswith("TIF")):
         print(file)
         lyr = QgsRasterLayer("citra"+"\\"+folder+"\\"+file)
         ras = QgsRasterCalculatorEntry()
@@ -51,17 +52,17 @@ for file in os.listdir("citra"+"\\"+folder):
         iface.addRasterLayer("citra"+"\\"+folder+"\\"+file, file.split("_")[-1][:-4])
 
 
-for type in types[0:1]:
-    calc = QgsRasterCalculator(rumus[type], type+'.tif', 'GTiff', lyr.extent(), lyr.width(), lyr.height(),entries )
-    calc.processCalculation()
-#     processing.run("gdal:rastercalculator", {
-#         'INPUT_A':'citra/LC08_L2SP_118065_20130728_20200912_02_T1/LC08_L2SP_118065_20130728_20200912_02_T1_SR_B2.TIF','BAND_A':1,
-#         'INPUT_B':'citra/LC08_L2SP_118065_20140629_20200911_02_T1/LC08_L2SP_118065_20140629_20200911_02_T1_SR_B4.TIF','BAND_B':1,
-#         'INPUT_C':'citra/LC08_L2SP_118065_20130728_20200912_02_T1/LC08_L2SP_118065_20130728_20200912_02_T1_SR_B5.TIF','BAND_C':1,
-#         'INPUT_D':'citra/LC08_L2SP_118065_20130728_20200912_02_T1/LC08_L2SP_118065_20130728_20200912_02_T1_SR_B6.TIF','BAND_D':1,
-#         'INPUT_E':'citra/LC08_L2SP_118065_20140629_20200911_02_T1/LC08_L2SP_118065_20140629_20200911_02_T1_SR_B7.TIF','BAND_E':1,
-#         'INPUT_F':'citra/LC08_L2SP_118065_20130728_20200912_02_T1/LC08_L2SP_118065_20130728_20200912_02_T1_ST_B10.TIF','BAND_F':1,
-#         'FORMULA':rumusGDAL[type],'NO_DATA':None,'PROJWIN':None,'RTYPE':5,'OPTIONS':'','EXTRA':'',
-#    'OUTPUT': type+'.tif'})
+for type in types[0:4]:
+    processing.run("qgis:rastercalculator", {
+        'EXPRESSION':' ("B5@1" - "B4@1"  )  /  ( "B5@1" + "B4@1" ) ',
+        'LAYERS':[
+        "citra"+"\\"+folder+"\\"+folder+"_SR_B2.TIF",
+        "citra"+"\\"+folder+"\\"+folder+"_SR_B4.TIF",
+        "citra"+"\\"+folder+"\\"+folder+"_SR_B5.TIF",
+        "citra"+"\\"+folder+"\\"+folder+"_SR_B6.TIF",
+        "citra"+"\\"+folder+"\\"+folder+"_SR_B7.TIF",
+        "citra"+"\\"+folder+"\\"+folder+"_ST_B10.TIF",
+        ],
+        'CELLSIZE':0,'EXTENT':'682131.481400000,704231.481400000,-812652.242500000,-793452.242500000 [EPSG:32649]','CRS':None,'OUTPUT':type+'.tif'})
     iface.addRasterLayer(type+'.tif', type)
-    print(type)
+    print(rumusGDAL[type])
